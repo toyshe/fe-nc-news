778a3a669e88e85f6e2970d0bafdc1e02048b0d9
toyshe <toyshehossain@outlook.com>
2024-02-09 13:51:28 +0000

    Adding error handling and loading

--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import getAllArticles from "../utils/utils";
 import { useNavigate } from "react-router-dom";
 import SortArticles from "./SortArticles";
+import ErrorHandling from './ErrorHandling'
+import Loading from "./Loading";
 
 export default function ArticleList({
   articleList,
@@ -10,18 +12,27 @@ export default function ArticleList({
 }) {
   const [totalPages, setTotalPages] = useState(1);
   const [page, setPage] = useState(1);
-  const [sortBy, setSortBy] = useState('')
-  const [order, setOrder] = useState('')
+  const [sortBy, setSortBy] = useState("");
+  const [order, setOrder] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true)
   const navigate = useNavigate();
 
   useEffect(() => {
+    setLoading(true)
     getAllArticles(articleTopic).then(({ articles }) => {
       setTotalPages(Math.ceil(articles.length / 10));
     });
 
-    getAllArticles(articleTopic, sortBy, order, page).then(({ articles }) => {
-      setArticleList(articles);
-    });
+    getAllArticles(articleTopic, sortBy, order, page)
+      .then(({ articles }) => {
+        setArticleList(articles);
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false)
+      });
   }, [articleTopic, page, sortBy, order]);
 
   const handleArticleClick = (article) => {
@@ -38,6 +49,10 @@ export default function ArticleList({
       }`
     );
   };
+
+  if(error){return <ErrorHandling error={error} />}
+
+  if(loading){return <Loading loadingHeader='articles' />}
 
   return (
     <div className="article-list">
--- src/Components/TopicsSelect.jsx
+++ src/Components/TopicsSelect.jsx
@@ -1,21 +1,39 @@
 import { useEffect, useState } from "react";
 import { getTopics } from "../utils/utils";
+import { useSearchParams } from "react-router-dom"; // Importing useSearchParams
+import ErrorHandling from "./ErrorHandling";
 import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
 
 export default function TopicsSelect({ setArticleTopic }) {
   const [topicsList, setTopicsList] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true)
   const navigate = useNavigate();
 
   useEffect(() => {
-    getTopics().then(({ topics }) => {
-      setTopicsList(topics);
-    });
+    getTopics()
+      .then(({ topics }) => {
+        setTopicsList(topics);
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false)
+      });
   }, []);
 
   const handleTopicsSelect = (event) => {
     setArticleTopic(event.target.value);
     navigate(`/articles?topic=${event.target.value}`);
   };
+  if (error) {
+    return <ErrorHandling error={error} />;
+  }
+
+  if(loading){
+    return <Loading />
+  }
 
   return (
     <>
